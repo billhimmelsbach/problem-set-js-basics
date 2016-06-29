a2f58914d807da43e4d0ef636851f304a25a3e16
billhimmelsbach <whimmels@gmail.com>
2016-06-29 16:32:09 -0700

    bottles of beer problem complete

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -21,4 +21,31 @@
 
 */
 
-// YOUR CODE HERE
+// var bottleDown = function(i) {
+bottleDown = function(bottles) {
+for (var i=bottles; i > 0; i--) {
+  if (i>2) {
+    console.log(i + " bottles of beer on the wall,");
+    console.log(i + " bottles of beer!");
+    console.log("Take one down and pass it around,");
+    console.log(i-1 + " bottles of beer on the wall...");
+    console.log(" ");
+}
+  else if (i===2) {
+    console.log(i + " bottle of beer on the wall,");
+    console.log(i + " bottle of beer!");
+    console.log("Take one down and pass it around,");
+    console.log(i-1 + " bottle of beer on the wall...");
+    console.log(" ");
+  }
+
+  else {
+    console.log(i + " bottle of beer on the wall,");
+    console.log(i + " bottle of beer!");
+    console.log("Take one down and pass it around,");
+    console.log("No more bottles of beer on the wall...");
+    console.log(" ");
+  }
+}
+};
+bottleDown(5);
